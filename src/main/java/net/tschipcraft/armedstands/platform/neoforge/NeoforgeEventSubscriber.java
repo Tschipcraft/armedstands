94dcba83dfcf1842c79bb0012a1072c596064713
Tschipcraft <78470530+Tschipcraft@users.noreply.github.com>
2026-08-15 00:34:21 +0200

    feat(26.2): switch to `EntityInteract` event
    - Switch from `EntityInteractSpecific` to `EntityInteract` for NeoForge 26.2 (see https://github.com/neoforged/NeoForge/pull/3339 )
    - Bump mod version to `1.1`
    
    Ref: Minecraft 26.2 support

--- src/main/java/net/tschipcraft/armedstands/platform/neoforge/NeoforgeEventSubscriber.java
+++ src/main/java/net/tschipcraft/armedstands/platform/neoforge/NeoforgeEventSubscriber.java
@@ -12,8 +12,14 @@ import net.tschipcraft.armedstands.event.ArmorStandInteract;
 @EventBusSubscriber(modid = ArmedStands.MOD_ID)
 public class NeoforgeEventSubscriber {
 
+	// NeoForge 26.2 combined EntityInteractSpecific into EntityInteract
+	// (neoforged/NeoForge#3339, 26.2.0.43-beta)
 	@SubscribeEvent
+	//? if >=26.2 {
+	/^public static void onEntityInteract(PlayerInteractEvent.EntityInteract event) {
+	^///?} else {
 	public static void onEntityInteract(PlayerInteractEvent.EntityInteractSpecific event) {
+	//?}
 		InteractionResult result = ArmorStandInteract.onClick(event.getEntity(), event.getLevel(), event.getHand(), event.getTarget(), null);
 		if (result.consumesAction()) {
 			event.setCanceled(true);
